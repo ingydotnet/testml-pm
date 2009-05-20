dc2da6ecd7bd1f9753855ec21334b4c76aaaf6cc
Ingy döt Net <ingy@ingy.net>
2009-05-19 17:02:40 -0700

    Forgot to add TAP runner.

---
+++ lib/TestML/Runner/TAP.pm
@@ -0,0 +1,53 @@
+package TestML::Runner::TAP;
+use strict;
+use warnings;
+
+use TestML::Runner -base;
+
+use Test::Builder;
+
+field 'test_builder' => -init => 'Test::Builder->new';
+
+sub setup {
+    my $self = shift;
+    {
+        local @INC = ('t', 't/lib', @INC);
+        my $class = $self->bridge;
+        eval "require $class";
+        die $@ if $@;
+    }
+}
+
+sub title {
+    my $self = shift;
+    if (my $title = $self->doc->meta->get('Title')) {
+        print "=== $title ===\n";
+    }
+}
+
+sub plan_begin {
+    my $self = shift;
+    if (my $tests = $self->doc->meta->get('Plan')) {
+        $self->test_builder->plan(tests => $tests);
+    }
+    else {
+        $self->test_builder->no_plan();
+    }
+}
+
+sub plan_end {
+}
+
+# TODO - Refactor so that standard lib finds this comparison through EQ
+sub do_test {
+    my $self = shift;
+    my $left = shift;
+    my $operator = shift;
+    my $right = shift;
+    my $label = shift;
+    if ($operator eq '==') {
+        $self->test_builder->is_eq($left->value, $right->value, $label);
+    }
+}
+
+1;
