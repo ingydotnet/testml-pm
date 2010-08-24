882ddfca320b64232d20e49fad65993bac77c76f
Ingy dot Net <ingy@ingy.net>
2010-08-23 21:28:15 -0700

    Forgot to add the Debug library

---
+++ lib/TestML/Transforms/Debug.pm
@@ -0,0 +1,28 @@
+package TestML::Transforms::Debug;
+use TestML();
+
+sub WWW {
+    require XXX;
+    local $XXX::DumpModule = $TestML::DumpModule;
+    XXX::WWW(shift->value);
+}
+
+sub XXX {
+    require XXX;
+    local $XXX::DumpModule = $TestML::DumpModule;
+    XXX::XXX(shift->value);
+}
+
+sub YYY {
+    require XXX;
+    local $XXX::DumpModule = $TestML::DumpModule;
+    XXX::YYY(shift->value);
+}
+
+sub ZZZ {
+    require XXX;
+    local $XXX::DumpModule = $TestML::DumpModule;
+    XXX::ZZZ(shift->value);
+}
+
+1;
