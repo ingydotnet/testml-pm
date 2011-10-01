b78de754bf3367952ba806bd76677c77aa3b2c0c
Ingy dot Net <ingy@ingy.net>
2011-10-01 14:51:52 +0100

    new Mo::Inline

--- lib/TestML/Mo.pm
+++ lib/TestML/Mo.pm
@@ -6,26 +6,28 @@
 # copyright: 2010, 2011
 
 package TestML::Mo;
-# use Mo qw'default builder';
-no warnings;my$M=__PACKAGE__.::;*{$M.Object::new}=sub{bless{@_[1..$#_]},$_[0]};*{$M.import}=sub{import warnings;$^H|=1538;my($P,%e,%o)=caller.::;shift;eval"no Mo::$_",&{$M.$_.::e}($P,\%e,\%o)for@_;%e=(extends,sub{eval"no $_[0]()";@{$P.ISA}=$_[0]},has,sub{my$n=shift;my$m=sub{$#_?$_[0]{$n}=$_[1]:$_[0]{$n}};$m=$o{$_}->($m,$n,@_)for sort keys%o;*{$P.$n}=$m},%e,);*{$P.$_}=$e{$_}for keys%e;@{$P.ISA}=$M.Object};*{$M.'default::e'}=sub{my($P,$e,$o)=@_;$o->{default}=sub{my($m,$n,%a)=@_;$a{default}or return$m;sub{$#_?$m->(@_):!exists$_[0]{$n}?$_[0]{$n}=$a{default}->(@_):$m->(@_)}}};*{$M.'builder::e'}=sub{my($P,$e,$o)=@_;$o->{builder}=sub{my($m,$n,%a)=@_;my$b=$a{builder}or return$m;sub{$#_?$m->(@_):!exists$_[0]{$n}?$_[0]{$n}=$_[0]->$b:$m->(@_)}}};
+# use Mo qw'build default builder';
+#   The following line of code was produced from the previous line by
+#   Mo::Inline version 0.25
+no warnings;my$M=__PACKAGE__.::;*{$M.Object::new}=sub{bless{@_[1..$#_]},$_[0]};*{$M.import}=sub{import warnings;$^H|=1538;my($P,%e,%o)=caller.::;shift;eval"no Mo::$_",&{$M.$_.::e}($P,\%e,\%o)for@_;%e=(extends,sub{eval"no $_[0]()";@{$P.ISA}=$_[0]},has,sub{my$n=shift;my$m=sub{$#_?$_[0]{$n}=$_[1]:$_[0]{$n}};$m=$o{$_}->($m,$n,@_)for sort keys%o;*{$P.$n}=$m},%e,);*{$P.$_}=$e{$_}for keys%e;@{$P.ISA}=$M.Object};*{$M.'build::e'}=sub{my($P,$e)=@_;$e->{new}=sub{$c=shift;my$s=bless{@_},$c;my@B;do{@B=($c.::BUILD,@B)}while($c)=@{$c.::ISA};exists&$_&&&$_($s)for@B;$s}};*{$M.'default::e'}=sub{my($P,$e,$o)=@_;$o->{default}=sub{my($m,$n,%a)=@_;$a{default}or return$m;sub{$#_?$m->(@_):!exists$_[0]{$n}?$_[0]{$n}=$a{default}->(@_):$m->(@_)}}};*{$M.'builder::e'}=sub{my($P,$e,$o)=@_;$o->{builder}=sub{my($m,$n,%a)=@_;my$b=$a{builder}or return$m;sub{$#_?$m->(@_):!exists$_[0]{$n}?$_[0]{$n}=$_[0]->$b:$m->(@_)}}};use strict;use warnings;
 
-use strict;
-use warnings;
-no strict 'refs';
-no warnings 'redefine';
+{
+    no strict 'refs';
+    no warnings 'redefine';
 
-my $import = \&{__PACKAGE__ . '::import'};
-*import = sub {
-    my $p = caller;
-    if (not defined &{$p.'::XXX'}) {
-        *{$p.'::WWW'} = \&WWW;
-        *{$p.'::XXX'} = \&XXX;
-        *{$p.'::YYY'} = \&YYY;
-        *{$p.'::ZZZ'} = \&ZZZ;
-    }
-    push @_, qw(builder default);
-    goto &$import;
-};
+    my $import = \&{__PACKAGE__ . '::import'};
+    *import = sub {
+        my $p = caller;
+        if (not defined &{$p.'::XXX'}) {
+            *{$p.'::WWW'} = \&WWW;
+            *{$p.'::XXX'} = \&XXX;
+            *{$p.'::YYY'} = \&YYY;
+            *{$p.'::ZZZ'} = \&ZZZ;
+        }
+        push @_, qw(build builder default);
+        goto &$import;
+    };
+}
 
 use constant XXX_skip => 1;
 our $DumpModule = 'YAML::XS';
